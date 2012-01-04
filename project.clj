7ef6ea51ff0e9ddc463a511d9f9160dbf70059be
Geoff Salmon <geoff.salmon@gmail.com>
2012-01-04 13:23:08 -0500

    update dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject ring-bake "0.1.0-SNAPSHOT"
+(defproject ring-bake "0.2.0"
   :description "Library for baking static websites from Ring handlers"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [ring/ring-core "0.3.6"]
+                 [ring/ring-core "1.0.1"]
                  [ring-mock "0.1.1"]
-                 ]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+                 ])
